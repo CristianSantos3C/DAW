c358f99b9f54a53a6a16ad18c251f5ed12d1968a
CristianSantos3C <100966065+CristianSantos3C@users.noreply.github.com>
2022-07-19 08:37:42 -0600

    Delete ejemplodedocu.js

--- Guia1/Document.write/ejemplodedocu.js
+++
@@ -1,6 +0,0 @@
-document.write("<header class='headerstyle'><h1>Header</h1></header>");
-document.write("<nav class='navStyle'><h1>Navigation</h1></nav>")
-document.write("<article class='artStyle'><h1>Article</h1></article>")
-document.write("<article class='artStyle'><h1>Article</h1></article>")
-document.write("<article class='artStyle'><h1>Article</h1></article>")
-document.write("<footer class='footerStyle'><h1>Cristian Santos #20</h1></footer>")
